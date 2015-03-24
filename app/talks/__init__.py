23b7acc41adba2f031f76ccd6fb66f90a4b5ffc2
Manu <bionikspoon@gmail.com>
2015-03-24 17:37:38 -0500

    first

---
+++ app/talks/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+from flask import Blueprint
+
+talks = Blueprint('talks', __name__)
+from . import routes
---
+++ config.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+import os
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {'development': DevelopmentConfig,
+          'testing': TestingConfig,
+          'production': ProductionConfig,
+
+          'default': DevelopmentConfig}
