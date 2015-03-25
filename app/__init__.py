61d89b7ec3bb1cfc79a844d0c964ba4daee6fb03
Manu <bionikspoon@gmail.com>
2015-03-25 18:43:51 -0500

    flask moment dates

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask.ext.moment import Moment
 
 from config import config
 
@@ -11,7 +12,7 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-
+moment = Moment()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -20,6 +21,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    moment.init_app(app)
 
     from .talks import talks as talks_blueprint
     from .auth import auth as auth_blueprint
